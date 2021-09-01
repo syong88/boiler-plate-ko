89275163bd22f3971d340e30c49b9f236d41dcfc
syong88 <syong_88@naver.com>
2021-09-01 13:32:38 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose') 
+mongoose.connect('mongodb+srv://soyeong:abcd1234@boilerplate.k2gkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+    
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
